79db43a091607ecf65609aed4ba553a08541d2e7
Sholto Maud <sholto.maud@gmail.com>
2015-02-06 16:28:23 +1000

    adding topofy functionality

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 var request = require('request'),
 	hosts = require('./config/config.json'),
+	through = require('through'),
 	topofy = require('./topofier'),
-	http = require('http');
+	http = require('http')
+	geojson = require('./geojson.json');
 
 http.createServer(function (req, res) {
 	var url = req.url;
-	url = url.replace(/\//,'').replace(/%7B/g,'{').replace(/%7D/g,'}');
-	
+	url = url.replace(/\//,'').replace(/%7B/g,'{').replace(/%7D/g,'}').replace(/%22/g,'"');
+	//console.log(JSON.parse(url));
 	// var query = hosts.nrm.host + '?' + url + "&userid=363708495";
-	
-   	res.removeHeader("Cache-Control");
+	res.removeHeader("Cache-Control");
 	res.setHeader("Content-Type", "application/json; charset=utf-8");
 	res.setHeader("Access-Control-Allow-Origin", "*");
+	console.log('ulr [',url,']');
+	if (req.method === 'GET') {
+		//console.log('line [',geojson,']');
+        req
+        	.pipe( through(function (buf) {
+	            var line = buf.toString();
+			    console.log('line [',geojson,']');
+			    console.log('line [',line,']');
+			    this.queue(geojson);
+	        }))
+	        .pipe(res)
+	        .on('error', function(err) {
+		    	console.log(err);
+			})
+        //console.log(res);
+    }
+    else res.end(console.log('send me a GET\n'));
 
-	hosts.checkBounds();
+/*
+	req
+	 	.get(query,function(){
+	 		topofy.geofy(query
+	 	})
+	 	.on('error', function(err) {
+	    	console.log(err)
+
+	  	})
+	pipe( function(body){
+
+	}));
+	console.log(topofy.geofy.pipe(req));
+*/
+   	
+	//hosts.checkBounds();
 
 	/*
 	request
