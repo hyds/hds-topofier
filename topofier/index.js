ebc43c2c9c6528e349f1485af16380c1b2e957ee
Sholto Maud <sholto.maud@gmail.com>
2015-06-03 17:40:11 +1000

    elev

--- topofier/index.js
+++ topofier/index.js
@@ -1,6 +1,7 @@
 var stream = require('stream');
 var util = require('util');
 var numeral = require('numeral');
+var moment = require('moment');
 // node v0.10+ use native Transform, else polyfill
 var Transform = stream.Transform;
 
@@ -45,14 +46,45 @@ Geofy.prototype._transform = function (buf, enc, cb) {
 
         feature.geometry.coordinates = coordinates;
         //console.log("station ["+sites[i].station+"]")
+        var elev = sites[i].elev || "UNKNOWN";
         var stname = sites[i].stname || "UNKNOWN";
         var station = sites[i].station || "UNKNOWN";
         var stntype =sites[i].stntype || "UNKNOWN";
         var lat =sites[i].latitude || "UNKNOWN";
         var lon =sites[i].longitude || "UNKNOWN";
+        var complianceStatus = Math.random();
+        var complianceIcon;
+
+        if ( complianceStatus > 0.7 ) {
+            complianceIcon = '&#10060'; 
+        }
+        else if ( complianceStatus < 0.3 ){
+            complianceIcon = '&#10071'; 
+        }
+        else{
+            complianceIcon = '&#9989'; 
+        }
+
+        var assesmentDate = moment().subtract(complianceStatus * 1000, 'days').format("MMM Do YY"); 
+
         feature.properties.name = stname;
         feature.properties.amenity = stntype;
-        feature.properties.popupContent = "<b>Site ID:</b> " + station + "</br><b>Name:</b> " + stname + "</br><b>Lat:</b> " + lat + "</br><b>Long:</b> " + lon + "</br><b>Type:</b> " + stntype + "</br><b>Technical Risk:</b> " + numeral(Math.round(Math.random()*100000)).format('($ 0.00 a)') + "</br><b>Env. Risk:</b> " + numeral(Math.round(Math.random()*10000000)).format('($ 0.00 a)') + "</br><b>Cumulative Impact:</b> " + numeral(Math.round(Math.random()*100000000)).format('($ 0.00 a)') + "</br><b>Hedge Book Risk:</b> " + numeral(Math.round(Math.random()*1000000)).format('($ 0.00 a)') ;
+
+        feature.properties.popupContent = '<table style="width:100%">'+
+        '<col align="right">'+
+        '<col align="left">'+
+        '<tr><td align="right"><b>Site ID:</b></td> <td>' + station + '</td></tr>'+
+        '<tr><td align="right"><b>Name:</b></td> <td>' + stname + '</td></tr>'+
+        '<tr><td align="right"><b>Lat:</b></td> <td>' + lat + '</td></tr>'+
+        '<tr><td align="right"><b>Long:</b></td> <td>' + lon + '</td></tr>'+
+        '<tr><td align="right"><b>Elevation:</b></td> <td>' + elev + '</td></tr>'+
+        '<tr><td align="right"><b>WQ Compliance:</b></td> <td>' + complianceIcon + '</td></tr>'+
+        '<tr><td align="right"><b>Technical Risk:</b></td> <td>' + numeral(Math.round(Math.random()*100000)).format('($ 0.00 a)') + '</td></tr>'+
+        '<tr><td align="right"><b>Env. Risk:</b></td> <td>' + numeral(Math.round(Math.random()*10000000)).format('($ 0.00 a)')  + '</td></tr>'+
+        '<tr><td align="right"><b>Assess Date:</b></td> <td>' + assesmentDate + '</td></tr>'+
+        '<tr><td align="right"><b>Cumulative Impact:</b></td> <td>' +  numeral(Math.round(Math.random()*100000000)).format('($ 0.00 a)')  + '</td></tr>'+
+        '<tr><td align="right"><b>Hedge Book Risk:</b></td> <td>' + numeral(Math.round(Math.random()*1000000)).format('($ 0.00 a)') + '</td></tr>'+
+        '</table>';//+'First name: <input type="text" name="fname"><br>Last name: <input type="text" name="lname"><br><input type="submit" value="Submit">"';
 
 /*
         {"station": "070048", "longitude": "149.44790000", "stname": "HOSKINTOWN RADIO
